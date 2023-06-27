55cd0da114df13ea78504f3ecd0872d7392b3ab9
rega1237 <rega1237@gmail.com>
2023-06-26 20:55:58 -0400

    2697 leetcode javascript solution

---
+++ Leetcode/2697. Lexicographically Smallest Palindrome/solution.js
@@ -0,0 +1,28 @@
+const makeSmallestPalindrome = (s) => {
+  let left = 0
+  let right = s.length - 1
+
+  let stringPal = s.split("")
+
+  while(left < right) {
+    const charLeft = s[left]
+    const charRight = s[right]
+
+    if(charLeft != charRight) {
+      if(charLeft > charRight)
+        stringPal[left] = charRight
+      else{
+        stringPal[right] = charLeft
+      }
+      left ++
+      right --
+    } else {
+      left ++
+      right --
+    }
+  }
+
+  return stringPal.join("")
+}
+
+console.log(makeSmallestPalindrome("egcfe")) // "efcfe"
